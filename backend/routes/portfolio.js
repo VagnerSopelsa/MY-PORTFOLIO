fbfcf2172034d53abb3525797387e504fca157fd
Vagner Sopelsa <vagner.sopelsa@gmail.com>
2021-11-30 00:18:32 -0300

    add bodyParser

---
+++ backend/routes/portfolio.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    //acess db
+    const data = [ {
+        id: 1,
+        name: 'My fist project',
+        createdAt: '2020-12-01'
+    },
+    {
+        id: 2,
+        name: 'My second project',
+        createdAt: '2020-01-06'
+    },
+    {
+        id: 3,
+        name: 'My other project',
+        createdAt: '2021-01-25'
+    },
+];
+
+    res.json({
+        sucess: true,
+        data
+    });
+});
+
+router.get('/:detalhe' , (req, res) => {
+    console.log("Este é o id informado:", req.params.detalhe);
+
+    res.json({
+        success: true,
+        id: req.params.detalhe
+    })
+})
+
+router.post('/',(req, res) => {
+    res.json(req.body)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const api = require('./backend/routes');
 
 const app = express();
 
-const PORT = 3080;
+app.use(bodyParser.json())
 
-app.get('/',(req, res)=> {
+app.get('/',(req, res) => {
     res.json({
         'sucess':true
     });
 });
 
-app.get('/portfolio', (req, res) => {
-    //acess db
-    const data = [{
-        id: 1,
-        name: 'My fist project',
-        createdAt: '2020-12-01'
-    },
-    {
-        id: 2,
-        name: 'My second project',
-        createdAt: '2020-01-06'
-    },
-    {
-        id: 3,
-        name: 'My other project',
-        createdAt: '2021-01-25'
-    },
-    ];
+app.use('/api', api);
 
-    res.json({
-        sucess: true,
-        data
-    })
-})
+const PORT = 3080;
 app.listen(PORT);
